ab93a1ef8f60e6c7cff2887895468f15c85cd782
JesterlyKing <iswashin@mtu.edu>
2023-10-03 16:24:32 -0400

    Raft creation and added enemy art

---
+++ Bermuda Adrift/Assets/Scripts/Classes/Grid.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid : MonoBehaviour
+{
+    private int width;
+    private int height;
+    private float cellSize;
+    private Vector3 originPosition;
+    private int[,] gridArray;
+
+    public Grid(int width, int height, float cellSize, Vector3 originPosition)
+    {
+        this.width = width;
+        this.height = height;
+        this.cellSize = cellSize;
+        this.originPosition = originPosition;
+
+        gridArray = new int[width, height];
+
+        for (int y = 0; y < gridArray.GetLength(1); y++)
+        {
+            for (int x = 0; x < gridArray.GetLength(0); x++)
+            {
+                Debug.DrawLine(GetWorldPosition(x, y), GetWorldPosition(x, y + 1), Color.white, 100f);
+                Debug.DrawLine(GetWorldPosition(x, y), GetWorldPosition(x + 1, y), Color.white, 100f);
+            }
+        }
+    }
+
+    private Vector3 GetWorldPosition(int x, int y)
+    {
+        return new Vector3(x, y) * cellSize + originPosition;
+    }
+}
